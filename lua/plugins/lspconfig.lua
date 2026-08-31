64ccc1f490cae8bb1476908f2e7da59ce36876f6
Clarence <tranvantien080303@gmail.com>
2026-08-31 10:25:13 +0700

    update

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,15 +2,14 @@ return {
   'neovim/nvim-lspconfig',
 
   config = function()
+    local telescope = require('telescope.builtin')
+
+    -- Diagnostics
     vim.diagnostic.config {
       virtual_text = true,
-
       signs = true,
-
       underline = true,
-
       update_in_insert = false,
-
       severity_sort = true,
 
       float = {
@@ -28,26 +27,31 @@ return {
           silent = true,
         }
 
+        -- LSP
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.type_definition, opts)
 
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
 
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        -- Find References
+        -- Hiển thị tất cả nơi symbol được sử dụng trong Telescope
+        vim.keymap.set('n', 'gr', telescope.lsp_references, opts)
 
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        -- Rename
+        vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
 
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        -- Code Action
+        vim.keymap.set('n', '`', vim.lsp.buf.code_action, opts)
 
+        -- Diagnostics
         vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
 
         vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 
         vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
+        -- Toggle Inlay Hint
         vim.keymap.set('n', '<leader>ih', function()
           vim.lsp.inlay_hint.enable(
             not vim.lsp.inlay_hint.is_enabled()
