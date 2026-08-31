af40253a9374b10bb4aa555ede31ab8eacc3860f
Clarence <tranvantien080303@gmail.com>
2026-08-31 15:00:46 +0700

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,6 +5,114 @@ return {
 
   opts = {
     flavour = 'mocha',
+    custom_highlights = function()
+      return {
+		Normal = {
+          bg = '#141414',
+        },
+
+        NormalNC = {
+          bg = '#141414',
+        },
+
+        NormalFloat = {
+          bg = '#141414',
+        },
+
+        EndOfBuffer = {
+          bg = '#141414',
+        },
+
+        SignColumn = {
+          bg = '#141414',
+        },
+
+        FoldColumn = {
+          bg = '#141414',
+        },
+
+        FloatNormal = {
+          bg = '#141414',
+        },
+
+        FloatBorder = {
+          bg = '#141414',
+          fg = '#3A3A3A',
+        },
+
+        TelescopeNormal = {
+          bg = '#141414',
+        },
+
+        TelescopeBorder = {
+          bg = '#141414',
+          fg = '#3A3A3A',
+        },
+
+        TelescopePromptNormal = {
+          bg = '#141414',
+        },
+
+        TelescopePromptBorder = {
+          bg = '#141414',
+          fg = '#3A3A3A',
+        },
+
+        TelescopeResultsNormal = {
+          bg = '#141414',
+        },
+
+        TelescopeResultsBorder = {
+          bg = '#141414',
+          fg = '#3A3A3A',
+        },
+
+        TelescopePreviewNormal = {
+          bg = '#141414',
+        },
+
+        TelescopePreviewBorder = {
+          bg = '#141414',
+          fg = '#3A3A3A',
+        },
+
+        TelescopeSelection = {
+          bg = '#252525',
+          fg = '#FFFFFF',
+        },
+
+        TelescopeSelectionCaret = {
+          bg = '#252525',
+          fg = '#75D175',
+        },
+
+        ['@lsp.type.method.cs'] = {
+          fg = '#DCDCAA',
+        },
+        ['@lsp.type.class.cs'] = {
+          fg = '#4EC9B0',
+        },
+        ['@lsp.type.struct.cs'] = {
+          fg = '#75D175',
+        },
+        ['@lsp.type.field.cs'] = {
+          fg = '#D69D85',
+        },
+        ['@lsp.type.property.cs'] = {
+          fg = '#DCDCDC',
+        },
+        ['@lsp.type.keyword.cs'] = {
+          fg = '#569CD6',
+        },
+        ['@lsp.type.parameter.cs'] = {
+          fg = '#9CDCFE',
+          italic = true,
+        },
+        ['@lsp.type.namespace.cs'] = {
+          italic = false,
+        },
+      }
+    end,
   },
 
   config = function(_, opts)
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,62 @@
 return {
   'nvim-lualine/lualine.nvim',
+
   config = function()
+    local function short_filename()
+      local path = vim.fn.expand('%:p')
+
+      if path == '' then
+        return '[No Name]'
+      end
+
+      local parts = vim.split(path, '[\\/]')
+      local count = #parts
+
+      if count <= 3 then
+        return table.concat(parts, '/')
+      end
+
+      return table.concat({
+        parts[count - 2],
+        parts[count - 1],
+        parts[count],
+      }, '/')
+    end
+
     require('lualine').setup {
+      options = {
+        theme = 'solarized_dark',
+      },
+
       sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { { 'filename', path = 3 } },
-        lualine_c = { 'diagnostics' },
+        lualine_a = {
+          'mode',
+        },
+
+        lualine_b = {
+          short_filename,
+        },
+
+        lualine_c = {
+          'diagnostics',
+        },
+
         lualine_x = {},
+
         lualine_y = {},
-        lualine_z = { 'filetype' },
+
+        lualine_z = {
+          'filetype',
+          {
+            'datetime',
+            style = '%d/%m %H:%M',
+            color = {
+			fg = '#FFFFFF',
+			bg = '#202020',
+			gui = 'bold',
+			},
+          },
+        },
       },
     }
   end,
