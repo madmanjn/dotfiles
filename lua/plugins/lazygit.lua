e2d3957cccbe95f14aed9e57e492a03d9565344f
Clarence <tranvantien080303@gmail.com>
2026-09-01 10:56:41 +0700

    add lazygit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,15 @@
+return {
+  'kdheepak/lazygit.nvim',
+
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+
+  keys = {
+    {
+      '<leader>lg',
+      '<cmd>LazyGit<cr>',
+      desc = 'LazyGit',
+    },
+  },
+}
